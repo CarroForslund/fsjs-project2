dedc7837717dec9d3ad156569eebad1460d1da21
Caroline Forslund <cf222ev@student.lnu.se>
2017-10-04 23:12:22 +0200

    Everything works
    Only refactoring left :)

--- js/script.js
+++ js/script.js
@@ -1,31 +1,37 @@
-/* When the page loads,
-** your program should hide all but the first 10 students in the list.
-*/
 const pageDiv = document.getElementsByClassName('page')[0]; //div with class "page"
 const pageHeader = document.getElementsByClassName('page-header')[0];
 const studentsArray = document.querySelectorAll('.student-item'); //Array of students (li)
-const students = []; //sorted student array
-let searchResult = [];
-let paginationDiv = null; //class pagination
+const students = []; //organized student array
 
-//clean up studentList and save to students
-function sortStudentList () {
+/* NEW CLEAN ARRAY OF STUDENTS
+** Clean up studentsArray and save to new array called studentsArray
+** (Maybe unnessecary, but the best solution I could come up with for now)
+*/
+function organizeStudentList () {
+
   for (let i = 0; i < studentsArray.length; i++){
+    //Get every student's information
     const studentName = studentsArray[i].querySelector('h3').innerHTML;
     const studentEmail = studentsArray[i].querySelector('span').innerHTML;
     const studentAvatar = studentsArray[i].querySelector('img').src;
     const studentJoined = studentsArray[i].querySelector('div:nth-child(2)').querySelector('span').innerHTML;
 
+    //Create a student object
     const student = {
       name: studentName,
       email: studentEmail,
       avatar: studentAvatar,
       joined: studentJoined
     };
+    //Add student to the new array students
     students.push(student);
   };
 };
 
+/* ADD SEARCHBOX TO PAGE
+** Search for matching student names or student emails
+** Listens to enter key press and button click
+*/
 function searchBox(){
   const div = document.createElement('div');
   div.className = 'student-search';
@@ -38,9 +44,14 @@ function searchBox(){
     const value = input.value;
     var key = e.which || e.keyCode;
     if (key === 13) { // 13 is enter
-      console.log('enter search');
       input.value = '';
-      searchStudents(value);
+      //If search value is an empty string, return all students in default array
+      //Else search for matching students
+      if (value === ''){
+        showStudents(1, students);
+      } else {
+        searchStudents(value);
+      };
     };
   });
   div.appendChild(input);
@@ -48,7 +59,6 @@ function searchBox(){
   const button = document.createElement('button');
   button.innerHTML = 'Search';
   button.addEventListener('click', function(){
-    console.log('button search');
     const input = document.querySelector('input');
     const value = input.value;
     input.value = '';
@@ -58,31 +68,53 @@ function searchBox(){
 
 };
 
+/* SEARCH FUNCTION
+** Use input from the search box to find matching student names or student emails
+** Show result if there is at least one match, or return message if there isn't
+*/
 function searchStudents(input){
-  console.log(input);
 
+  //Will store matching students if there is any
+  let searchResult = [];
+
+  //Loop through students to see if there is a name or email including the search input
   for (let i = 0; i < students.length; i++){
     if (students[i].name.includes(input) || students[i].email.includes(input)){
       searchResult.push(students[i]);
     };
   };
-  console.log(searchResult);
-  paginationDiv.parentNode.removeChild(paginationDiv);
-  showStudents(1, searchResult);
-  searchResult = [];
 
-
+  //If there isn't a match return message
+  //Otherwise show the matching students
+  if (searchResult.length === 0){
+    const studentList = document.querySelector('.student-list');
+    studentList.parentNode.removeChild(studentList); //Hide HTML list from index
+    paginationDiv.parentNode.removeChild(paginationDiv); //reset pagination div
+    const div = document.createElement('div');
+    pageDiv.appendChild(div);
+    const p = document.createElement('div');
+    p.innerHTML = "Unfortunately we couldn't find any students matching your search."
+    div.appendChild(p);
+  } else {
+    paginationDiv.parentNode.removeChild(paginationDiv); //reset pagination div
+    showStudents(1, searchResult);
+    searchResult = [];
+  };
 };
 
+/* SHOW STUDENTS ON PAGE
+** Use chosen array of students
+** Print student list, maximum 10 students per page
+*/
 function showStudents(pageNumber, arrayOfStudents){
   const studentList = document.querySelector('.student-list');
   studentList.parentNode.removeChild(studentList); //Hide HTML list from index
 
-  //Display students
   const ul = document.createElement('ul');
   ul.className = 'student-list';
   pageDiv.appendChild(ul);
 
+  //Calculate the range of students to display
   startIndex = pageNumber*10-10;
   if (pageNumber*10 > arrayOfStudents.length){
     endIndex = arrayOfStudents.length;
@@ -90,6 +122,7 @@ function showStudents(pageNumber, arrayOfStudents){
     endIndex = pageNumber*10;
   };
 
+  //Print the calculated range of students to page
   for (let i = pageNumber*10-10; i < pageNumber*10 && i < arrayOfStudents.length; i++){
 
     const li = document.createElement('li');
@@ -124,22 +157,24 @@ function showStudents(pageNumber, arrayOfStudents){
     joinedDiv.appendChild(dateSpan);
 
   };
+
+  //Add pagination to page
   pagination(pageNumber, arrayOfStudents);
 
 };
 
-function calculateNumberOfPages(arrayOfStudents){
-  const numberOfPages = Math.ceil(arrayOfStudents.length/10); //Rounds up to closest int
-  return numberOfPages;
-};
-
-//Pagination
+/* PRINT PAGINATION LINKS
+** Pagination links to display at the bottom of the page
+** Makes it possible to navigate through the list of students if more than 10 is listed
+*/
 function pagination(pageNumber, arrayOfStudents) {
 
-  numberOfPages = calculateNumberOfPages(arrayOfStudents);
+  //Calculate how many pages are needed for the amount of students in array
+  const numberOfPages = Math.ceil(arrayOfStudents.length/10); //Rounds up to closest int
 
-  paginationDiv = document.createElement('div'); //class pagination
-  paginationDiv.className = 'pagination';
+  const paginationDiv = createElement('div', 'className', 'pagination')
+  // paginationDiv = document.createElement('div'); //class pagination
+  // paginationDiv.className = 'pagination';
   const ul = document.createElement('ul');
 
   for (let i = 1; i < numberOfPages+1; i++){
@@ -158,25 +193,32 @@ function pagination(pageNumber, arrayOfStudents) {
     ul.appendChild(li);
   };
 
-
   paginationDiv.appendChild(ul);
   pageDiv.appendChild(paginationDiv);
 
 };
 
-searchBox();
-sortStudentList();
-//showStudents(1);
-showStudents(1, students);
-
-/* CREATE ANY ELEMENT
+/* RUN PROGRAM FROM THIS FUNCTION
+** Create a search box
+** Organise students
+** Show students
 */
-//const createElement = (elementName, property, value) =>{
-// const createElement = (elementName) =>{
-//   const element = document.createElement(elementName);
-//   //element[property] = value;
-//   return element;
-// };
+function run(){
+  searchBox();
+  organizeStudentList();
+  showStudents(1, students); //default page number and array of student to show
+};
+
+run();
+
+
+/* CREATE ANY ELEMENT WITH A PROPERTY
+*/
+function createElement(elementName, property, value){
+  const element = document.createElement(elementName);
+  element[property] = value;
+  return element;
+};
 
 //Append element
 // function appendElement(parent, element){
