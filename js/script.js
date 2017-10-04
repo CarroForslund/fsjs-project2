a841cf83c1d209b2500f8d4b14140a2ee43ecd56
Caroline Forslund <cf222ev@student.lnu.se>
2017-10-04 21:15:47 +0200

    Search result shows

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ const pageDiv = document.getElementsByClassName('page')[0]; //div with class "pa
 const pageHeader = document.getElementsByClassName('page-header')[0];
 const studentsArray = document.querySelectorAll('.student-item'); //Array of students (li)
 const students = []; //sorted student array
-const searchResult = [];
+let searchResult = [];
+let paginationDiv = null; //class pagination
 
 //clean up studentList and save to students
 function sortStudentList () {
@@ -66,10 +67,14 @@ function searchStudents(input){
     };
   };
   console.log(searchResult);
+  paginationDiv.parentNode.removeChild(paginationDiv);
+  showStudents(1, searchResult);
+  searchResult = [];
+
 
 };
 
-function showStudents(pageNumber){
+function showStudents(pageNumber, arrayOfStudents){
   const studentList = document.querySelector('.student-list');
   studentList.parentNode.removeChild(studentList); //Hide HTML list from index
 
@@ -79,13 +84,13 @@ function showStudents(pageNumber){
   pageDiv.appendChild(ul);
 
   startIndex = pageNumber*10-10;
-  if (pageNumber*10 > students.length){
-    endIndex = students.length;
+  if (pageNumber*10 > arrayOfStudents.length){
+    endIndex = arrayOfStudents.length;
   } else {
     endIndex = pageNumber*10;
   };
 
-  for (let i = pageNumber*10-10; i < pageNumber*10 && i < students.length; i++){
+  for (let i = pageNumber*10-10; i < pageNumber*10 && i < arrayOfStudents.length; i++){
 
     const li = document.createElement('li');
     li.className = 'student-item cf';
@@ -97,16 +102,16 @@ function showStudents(pageNumber){
 
     const img = document.createElement('img');
     img.className = 'avatar';
-    img.src = students[i].avatar;
+    img.src = arrayOfStudents[i].avatar;
     studentDiv.appendChild(img);
 
     const nameH3 = document.createElement('h3');
-    nameH3.innerHTML = students[i].name;
+    nameH3.innerHTML = arrayOfStudents[i].name;
     studentDiv.appendChild(nameH3);
 
     const emailSpan = document.createElement('span');
     emailSpan.className = 'email';
-    emailSpan.innerHTML = students[i].email;
+    emailSpan.innerHTML = arrayOfStudents[i].email;
     studentDiv.appendChild(emailSpan);
 
     const joinedDiv = document.createElement('div');
@@ -115,18 +120,24 @@ function showStudents(pageNumber){
 
     const dateSpan = document.createElement('span');
     dateSpan.className = 'date';
-    dateSpan.innerHTML = students[i].joined;
+    dateSpan.innerHTML = arrayOfStudents[i].joined;
     joinedDiv.appendChild(dateSpan);
 
   };
-  pagination(pageNumber);
+  pagination(pageNumber, arrayOfStudents);
 
 };
 
-//Pagination
-function pagination(pageNumber) {
+function calculateNumberOfPages(arrayOfStudents){
+  const numberOfPages = Math.ceil(arrayOfStudents.length/10); //Rounds up to closest int
+  return numberOfPages;
+};
 
-  const numberOfPages = Math.ceil(studentsArray.length/10); //Rounds up to closest int
+//Pagination
+function pagination(pageNumber, arrayOfStudents) {
+
+  numberOfPages = calculateNumberOfPages(arrayOfStudents);
+
   paginationDiv = document.createElement('div'); //class pagination
   paginationDiv.className = 'pagination';
   const ul = document.createElement('ul');
@@ -141,7 +152,7 @@ function pagination(pageNumber) {
     }
     a.addEventListener('click', function(){
       paginationDiv.parentNode.removeChild(paginationDiv); //reset pagination div
-      showStudents(i);
+      showStudents(i, arrayOfStudents);
     });
     li.appendChild(a);
     ul.appendChild(li);
@@ -155,7 +166,8 @@ function pagination(pageNumber) {
 
 searchBox();
 sortStudentList();
-showStudents(1);
+//showStudents(1);
+showStudents(1, students);
 
 /* CREATE ANY ELEMENT
 */
